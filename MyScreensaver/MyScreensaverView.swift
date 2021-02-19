0d979f88880caf615f9e2c7129690bd43c6319c7
JeromeSchweitzer <jschweit@purdue.edu>
2021-02-18 22:26:02 -0500

    Trail of oranges

--- MyScreensaver/MyScreensaverView.swift
+++ MyScreensaver/MyScreensaverView.swift
@@ -18,15 +18,13 @@ class MyScreensaverView: ScreenSaverView {
     private var ballPosition: CGPoint = .zero
     private var ballVelocity: CGVector = .zero
 //    private let ballRadius: CGFloat = 15
-    private let ballRadius: CGFloat = 100
+    private let ballRadius: CGFloat = 30
     private var ballYAccel: CGFloat = -2.0
     private var normalDamping: CGFloat = 0.7
     private var alternateDamping: CGFloat = 0.9
-    private var minVeloMargin: CGFloat = 1
-//    private var backgroundColor: NSColor = NSColor(red: 227.0, green: 83.0, blue: 73.0, alpha: 1.0)
     private var backgroundColor: NSColor = .systemBlue
-    private var balls: Array<CGPoint> = []
-    private var iterations: Int = 0
+//    let myImg = NSImage(contentsOf: URL(string: "https://cdn.d1baseball.com/logos/teams/256/purdue.png")!)
+    let myImg = NSImage(contentsOf: URL(string: "https://i.pinimg.com/originals/9f/cb/f7/9fcbf766603ed3d1e6c2808ee63ff3f3.png")!)
     
 
     // MARK: - Initialization
@@ -65,21 +63,22 @@ class MyScreensaverView: ScreenSaverView {
         }
         
         if ballVelocity.dy.magnitude <= 0.1 && ballVelocity.dx.magnitude <= 0.1 {
-//            backgroundColor = backgroundColor == .red ? .white : .red
+            // Ball has essentially come to a stop
             initializeBall()
         }
         
-        let tempPoint = CGPoint(x: ballPosition.x, y: ballPosition.y)
-        balls.append(_: tempPoint)
-        
-        setNeedsDisplay(bounds)
+        // Only updating in the current rectangle
+        setNeedsDisplay(NSRect(x: ballPosition.x - ballRadius,
+                               y: ballPosition.y - ballRadius,
+                               width: ballRadius * 2,
+                               height: ballRadius * 2))
+//        setNeedsDisplay(bounds)
     }
     
     // MARK: - Lifecycle
     // Draw a single frame
     override func draw(_ rect: NSRect) {
         drawBackground(backgroundColor)
-//        drawBalls()
         drawBall()
     }
     
@@ -89,23 +88,19 @@ class MyScreensaverView: ScreenSaverView {
         background.fill()
     }
     
-    private func drawBalls() {
-        for ball in balls {
-            let ballRect = NSRect(x: ball.x - ballRadius,
-                                  y: ball.y - ballRadius,
-                                  width: ballRadius * 2,
-                                  height: ballRadius * 2)
+//    private func drawBalls() {
+//        for ball in balls {
+//            let ballRect = NSRect(x: ball.x - ballRadius,
+//                                  y: ball.y - ballRadius,
+//                                  width: ballRadius * 2,
+//                                  height: ballRadius * 2)
 //            let ball = NSBezierPath(roundedRect: ballRect,
 //                                    xRadius: ballRadius,
 //                                    yRadius: ballRadius)
 //            NSColor.black.setFill()
 //
 //            ball.fill()
-            let myImg = NSImage(contentsOf: URL(string: "https://cdn.d1baseball.com/logos/teams/256/purdue.png")!)
-            myImg?.draw(in: ballRect)
-            // ^ Seems to take up a bunch of computing power
-        }
-    }
+//    }
     
     private func drawBall() {
         let ballRect = NSRect(x: ballPosition.x - ballRadius,
@@ -117,25 +112,15 @@ class MyScreensaverView: ScreenSaverView {
 //                                yRadius: ballRadius)
 //        NSColor.black.setFill()
 //        ball.fill()
-//        let myUrl = URL(string: "https://cdn.d1baseball.com/logos/teams/256/purdue.png")
-        let myImg = NSImage(contentsOf: URL(string: "https://cdn.d1baseball.com/logos/teams/256/purdue.png")!)
-//        let myImg = NSImage(contentsOfFile: "orange.png")
-//        let stringPath = Bundle.main.path(forResource: "orange", ofType: "png")
-//        let myImg = NSImage(named: "orange.jpg")
         myImg?.draw(in: ballRect)
         
     }
     
     private func initializeBall() {
-        iterations += 1
         ballPosition = CGPoint(x: frame.width/2, y: frame.height/2)
         ballVelocity = CGVector(dx: Double.random(in: -20.0...20.0), dy: Double.random(in: 30.0...40.0))
         normalDamping = 0.7
         alternateDamping = 0.9
-        if iterations >= 6 {
-            balls = []
-            iterations = 1
-        }
     }
     
     // Is the ball out of bounds in the x or y-axis
@@ -149,25 +134,3 @@ class MyScreensaverView: ScreenSaverView {
     }
 
 }
-//
-//class MyView: NSView {
-//
-//    var image : NSImage? {
-//        didSet {
-//           needsDisplay = true
-//        }
-//    }
-//
-//    init(frame frameRect: NSRect, image : NSImage?) {
-//        self.image = image
-//        super.init(frame: frameRect)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
-//
-//    override func draw(_ dirtyRect: NSRect) {
-//        image?.draw(at: .zero, from: bounds, operation: .sourceOver, fraction: 1.0)
-//    }
-//}
